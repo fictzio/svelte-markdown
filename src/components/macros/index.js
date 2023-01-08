5df12707d60c26e08f4ac8da2207e746eecac95b
Anders Olsen <andersnolsen@gmail.com>
2023-01-08 20:08:49 +0100

    Testing 1-2-3

--- src/components/macros/index.js
+++ src/components/macros/index.js
@@ -3,6 +3,11 @@ export { default as Random } from './Random.svelte'
 export { default as SetValue } from './SetValue.svelte'
 export { default as Goto } from './Goto.svelte'
 
+import Roll from './Roll.svelte'
+import Random from './Random.svelte'
+import SetValue from './SetValue.svelte'
+import Goto from './Goto.svelte'
+
 export const macroComponents = {
     roll: Roll,
     random: Random,
