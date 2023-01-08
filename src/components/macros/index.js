f381d07c11e51908bafa9abc472ed7a532a7d818
Anders Olsen <andersnolsen@gmail.com>
2023-01-08 20:12:52 +0100

    Last try

--- src/components/macros/index.js
+++ src/components/macros/index.js
@@ -3,14 +3,14 @@ export { default as Random } from './Random.svelte'
 export { default as SetValue } from './SetValue.svelte'
 export { default as Goto } from './Goto.svelte'
 
-import Roll from './Roll.svelte'
-import Random from './Random.svelte'
-import SetValue from './SetValue.svelte'
-import Goto from './Goto.svelte'
+import Roll__SvelteComponent_ from './Roll.svelte'
+import Random__SvelteComponent_ from './Random.svelte'
+import SetValue__SvelteComponent_ from './SetValue.svelte'
+import Goto__SvelteComponent_ from './Goto.svelte'
 
 export const macroComponents = {
-    roll: Roll,
-    random: Random,
-    set: SetValue,
-    goto: Goto
+    roll: Roll__SvelteComponent_,
+    random: Random__SvelteComponent_,
+    set: SetValue__SvelteComponent_,
+    goto: Goto__SvelteComponent_
 }
