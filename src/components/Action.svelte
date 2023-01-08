4a482921f8094425b61418b84fbd673a20478bd1
Anders Olsen <andersnolsen@gmail.com>
2023-01-08 15:59:39 +0100

    Testing special link handling

---
+++ src/components/Action.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let href = ''
+    export let title = undefined
+
+    function runAction() {
+        console.log(href)
+    }
+</script>
+
+<button on:click={runAction}>{title}</button>
--- src/renderers/Link.svelte
+++ src/renderers/Link.svelte
@@ -1,6 +1,20 @@
 <script>
+    import { isAsyncFunction } from "util/types"
+  import Action from "../components/Action.svelte";
+
   export let href = ''
   export let title = undefined
+
+  function isAction(txt) {
+    let macros = []
+    console.log(txt)
+    return false
+  }
+
 </script>
 
+{#if isAction(href)}
+<Action {href} {title} />
+{:else}
 <a {href} {title}><slot></slot></a>
+{/if}
