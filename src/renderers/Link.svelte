907920e0d8f0684e823d4c1634bcdd1a0c40ebb2
Anders Olsen <andersnolsen@gmail.com>
2023-01-08 17:24:58 +0100

    Why not work?

--- src/renderers/Link.svelte
+++ src/renderers/Link.svelte
@@ -1,23 +1,22 @@
-<script>
+<script lang="ts">
   import Action from "../components/Action.svelte";
 
-  export let href = ''
-  export let title = undefined
+  export let href:string|undefined
+  export let title:string|undefined = undefined
   
-  let params = undefined
-  let macro = undefined
-  let macros = ['/roll ','/add ','/tell ','/go ','/open ']
+  let params:string = ''
+  let macro:string = ''
+  let macros = ['roll ','add ','tell ','go ','open ']
 
-  let isMacro = function(txt) {
-		 if (macros.some(v => txt.includes(v))) {
+  let isMacro = function(txt:string) {
+		if (macros.some(v => txt.includes(v))) {
         params = txt.substring(txt.indexOf(' '), txt.length).trim()
 			  macro = txt.substring(0, txt.indexOf(' ')).trim()
-      return true
+        return true
     } else {
 			return false
 		}
 	 }
-
 </script>
 
 {#if isMacro(href)}
