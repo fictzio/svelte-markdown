378dce81b0f5698d3c4ace5ec3c85b3d38dc8689
Anders Olsen <andersnolsen@gmail.com>
2023-01-08 17:35:35 +0100

    Added basic component logic to paragraphs

---
+++ src/components/Dummy.svelte
@@ -0,0 +1,1 @@
+<p>COMPONENT GOES HERE</p>
--- src/renderers/Paragraph.svelte
+++ src/renderers/Paragraph.svelte
@@ -1,4 +1,20 @@
-<script>
-    console.log($$restProps)
+<script lang="ts">
+    import Dummy from '../components/Dummy.svelte'
+    export let text:string
+
+    function isComponent(txt:string):boolean {
+        let components = ['Component']
+        if (components.some(v => txt.includes(v)) && txt.indexOf(':')>-1) {
+            return true
+        } else {
+            return false
+        }
+    }
+    $:hasComponent = isComponent(text)
 </script>
+
+{#if hasComponent}
+<Dummy />
+{:else}
 <p><slot></slot></p>
+{/if}
