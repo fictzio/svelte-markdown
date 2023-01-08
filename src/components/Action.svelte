871693727754c87b278d5499e794f2a2a23e798c
Anders Olsen <andersnolsen@gmail.com>
2023-01-08 16:46:17 +0100

    Added macros to link

--- src/components/Action.svelte
+++ src/components/Action.svelte
@@ -1,10 +1,13 @@
 <script>
     export let href = ''
     export let title = undefined
+    export let params = undefined
+    let macro = href.substring(0, href.firstIndexOf(' '))
 
     function runAction() {
-        console.log('Action: ' + href)
+        console.log('Run macro: ' + macro)
+        console.log('Params: ' + params)
     }
 </script>
 
-<button on:click={runAction}>{title}</button>
+<button on:click={runAction} {title}><slot></slot></button>
--- src/renderers/Link.svelte
+++ src/renderers/Link.svelte
@@ -4,16 +4,21 @@
 
   export let href = ''
   export let title = undefined
+  
+  let params = undefined
 
   function isAction(txt) {
-    let macros = ['roll','add']
-    return macros.includes(txt)
+    let macros = ['/roll','/add','/tell','/go','/open']
+    let isMacro = macros.some(macro => txt.includes(macro));
+    
+    params = txt.substring(txt.firstIndexOf(' ') , txt.length)
+    return isMacro
   }
 
 </script>
 
 {#if isAction(href)}
-<Action {href} {title} />
+<Action {href} {title} {params}><slot></slot></Action>
 {:else}
 <a {href} {title}><slot></slot></a>
 {/if}
