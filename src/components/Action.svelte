681583de80a8b8aa210edfff33f719fddcb976c9
Anders Olsen <andersnolsen@gmail.com>
2023-01-08 17:53:18 +0100

    Rewriting Action component

--- src/components/Action.svelte
+++ src/components/Action.svelte
@@ -2,11 +2,14 @@
     export let title = undefined
     export let params = undefined
     export let macro = undefined
+    export let text = undefined
 
     function runAction() {
         console.log('Run macro: ' + macro)
         console.log('Params: ' + params)
     }
+
+    console.log(text)    
 </script>
 
 <button on:click={runAction} {title}><slot></slot> ({macro})</button>
